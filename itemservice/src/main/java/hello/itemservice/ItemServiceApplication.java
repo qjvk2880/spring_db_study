24a1706b746a6c6b4162871241655b7679d43c2e
qjvk2880 <qjvk2880@gmail.com>
2024-02-18 18:34:03 +0900

    Refactor : Mybatis Config를 사용하게끔 수정

--- itemservice/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -14,9 +14,11 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 
+
 @Slf4j
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
 //@Import(JdbcTemplateV1Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
